2986042ff8246277b62cb917e14de3a8b7e7102c
Asir <asiradnan23@gmail.com>
2024-05-19 12:02:52 +0600

    secured credentials

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+ 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,7 +134,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "verifywithasir@gmail.com"
-EMAIL_HOST_PASSWORD = "sscy lpsa tnqk hzsq"
+EMAIL_HOST_PASSWORD = os.environ.get('PASSWORD')
 UNIQUE_EMAIL = True
 ACCOUNT_EMAIL_REQUIRED = False
 ACCOUNT_EMAIL_VERIFICATION = 'optional'
