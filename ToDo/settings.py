7a5a4dd985949a822c00bece8505ec8cbef524c7
Asir <asiradnan23@gmail.com>
2024-05-19 15:57:17 +0600

    Added postgre

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'ToDo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER':'postgres',
+        'PASSWORD': os.environ.get('dbPASS'),
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '32799'
     }
 }
 
