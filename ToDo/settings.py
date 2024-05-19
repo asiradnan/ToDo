a944f596b89c68b505429e7a542c3145be84dd65
Asir <asiradnan23@gmail.com>
2024-05-19 13:13:54 +0600

    fix css loading

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -116,7 +116,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    os.path.join(BASE_DIR,'static')
 ]
 
 # Default primary key field type
